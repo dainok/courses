449fc032ca06a9dcde709754d5cf2ad97aba8cfa
Andrea Dainese <andrea.dainese@pm.me>
2024-04-10 16:38:29 +0200

    update

--- Ansible/cisco_ios-enable_telnet.py
+++ Ansible/cisco_ios-enable_telnet.py
@@ -1,30 +1,42 @@
 #!/usr/bin/env python3
-"""Connect to a Cisco IOS device via telnet and enable SSH."""
+"""
+Connect to a Cisco IOS device via telnet and enable SSH.
 
-# netmiko==4.3.0
+Depends on: pip install netmiko==4.3.0"""
 
+import sys
+import os
+import argparse
 from pprint import pprint
 from netmiko import ConnectHandler
-from getpass import getpass
 
-host = input("Enter hostname:       ")
-user = input("Enter username: ")
-password = getpass(prompt="Enter password: ")
+password = os.getenv("LEGACY_DEVICE_PASSWORD")
+secret = os.getenv("LEGACY_DEVICE_SECRET")
+if not password:
+    print("Set environment variable LEGACY_DEVICE_PASSWORD")
+    sys.exit(1)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--hostname', required=True, help="device hostname")
+parser.add_argument('--username', required=True, help="privileged username")
+args = parser.parse_args()
 
 device = {
-    "host": host,
-    "username": user,
+    "host": args.hostname,
+    "username": args.username,
     "password": password,
     "verbose": False,
 }
+if secret:
+    device["secret"] = secret
 
 output = ""
 net_connect = ConnectHandler(**device, device_type="cisco_ios_telnet")
-output += net_connect.send_config_set("ip domain name example.com")
+output += net_connect.send_config_set("ip domain name tndigit.it")
 output += net_connect.send_config_set("crypto key generate rsa modulus 2048")
 output += net_connect.send_config_set("ip ssh version 2")
 output += net_connect.send_config_set([
-    "line vty 0 4",
+    "line vty 0 15",
     "transport input telnet ssh",
 ])
 net_connect.disconnect()
