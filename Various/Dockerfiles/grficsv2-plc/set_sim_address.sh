1ec1a59675820a734215b9d269ec43566c533e30
Your Name <you@example.com>
2025-01-27 12:32:51 +0000

    fix openplc

--- Various/Dockerfiles/grficsv2-plc/set_sim_address.sh
+++ Various/Dockerfiles/grficsv2-plc/set_sim_address.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-CFG=/usr/local/src/OpenPLC/webserver/mbconfig.cfg
-DB=/usr/local/src/OpenPLC/webserver/openplc.db
+# CFG=/usr/local/src/OpenPLC/webserver/mbconfig.cfg
+CFG=/usr/local/src/OpenPLC/mbconfig.cfg
+# DB=/usr/local/src/OpenPLC/webserver/openplc.db
 
 sed -i "s/device0.address.*/device0.address = \"${SIM}\"/" ${CFG}
 sed -i "s/device1.address.*/device1.address = \"${SIM}\"/" ${CFG}
@@ -10,4 +11,4 @@ sed -i "s/device3.address.*/device3.address = \"${SIM}\"/" ${CFG}
 sed -i "s/device4.address.*/device4.address = \"${SIM}\"/" ${CFG}
 sed -i "s/device5.address.*/device5.address = \"${SIM}\"/" ${CFG}
 
-echo "UPDATE Slave_dev SET ip_address='${SIM};'" | sqlite3 ${DB}
+# #echo "UPDATE Slave_dev SET ip_address='${SIM};'" | sqlite3 ${DB}
