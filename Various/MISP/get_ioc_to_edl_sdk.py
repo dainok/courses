d30e664afa4cf99142af4dc8499740f0c5fdbc85
Andrea Dainese <andrea.dainese@pm.me>
2025-05-04 17:32:50 +0200

    update

---
+++ Various/MISP/get_ioc_to_edl_sdk.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+"""Export an EDL of IP addresses."""
+
+import logging
+import urllib3
+from datetime import datetime, timedelta
+from pymisp import PyMISP
+from http.server import BaseHTTPRequestHandler,HTTPServer
+import yaml
+import time
+
+logging.getLogger("pymisp").setLevel(logging.WARNING)
+
+with open("config.yml", "r") as fh:
+    config = yaml.safe_load(fh)
+
+last_days = datetime.now() - timedelta(days=config["export"]["last_days"])
+last_days_unix = int(time.mktime(last_days.timetuple()))
+
+# Disable SSL warning if cert validation is disabled
+if not config["misp"]["verify_cert"]:
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+misp = PyMISP(config["misp"]["url"], config["misp"]["key"], config["misp"]["verify_cert"], "json")
+
+# Search for IP addresses
+ip_list = []
+res = misp.search(
+    controller="attributes",
+    type_attribute=["ip-src", "ip-dst"],
+    timestamp=last_days_unix,
+    order="timestamp:desc",
+    limit=config["export"]["last_ioc"],
+    pythonify=False,
+)
+
+# Parse the result
+for item in res["Attribute"]:
+    if item["value"] not in ip_list:
+        ip_list.append(item["value"])
+ioc_page = "\n".join(ip_list).encode("utf-8")
+
+class pageHandler(BaseHTTPRequestHandler):
+    """Handle webserver requests."""
+    def do_GET(self):
+        """Handle GET requests."""
+        self.send_response(200)
+        self.send_header("Content-type","text/plain")
+        self.end_headers()
+        self.wfile.write(ioc_page)
+        return
+
+try:
+    # Start the webserver
+    server = HTTPServer(("0.0.0.0", 8080), pageHandler)
+    print("Webserver started")
+    server.serve_forever()
+except KeyboardInterrupt:
+    # Stop the webserver
+    print("Webserver stopped")
+    server.socket.close()
