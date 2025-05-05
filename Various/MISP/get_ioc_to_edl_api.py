3201cc0cdefc10e23ebac8c1e8d4286a4727842b
Andrea Dainese <andrea.dainese@pm.me>
2025-05-05 12:47:35 +0200

    update

--- Various/MISP/get_ioc_to_edl_api.py
+++ Various/MISP/get_ioc_to_edl_api.py
@@ -4,7 +4,7 @@
 import logging
 import urllib3
 from datetime import datetime, timedelta
-from pymisp import PyMISP
+import requests
 from http.server import BaseHTTPRequestHandler,HTTPServer
 import yaml
 import time
@@ -14,6 +14,16 @@ logging.getLogger("pymisp").setLevel(logging.WARNING)
 with open("config.yml", "r") as fh:
     config = yaml.safe_load(fh)
 
+params_post = {
+    "headers": {
+        "Authorization": config["misp"]["key"],
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+    },
+    "verify": config["misp"]["verify_cert"],
+    "timeout": config["misp"]["timeout"],
+}
+
 last_days = datetime.now() - timedelta(days=config["export"]["last_days"])
 last_days_unix = int(time.mktime(last_days.timetuple()))
 
@@ -21,27 +31,27 @@ last_days_unix = int(time.mktime(last_days.timetuple()))
 if not config["misp"]["verify_cert"]:
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-misp = PyMISP(config["misp"]["url"], config["misp"]["key"], config["misp"]["verify_cert"], "json")
-
 
 # Search for IP addresses
 ip_list = []
-res = misp.search(
-    controller="attributes",
-    type_attribute=["ip-src", "ip-dst"],
-    # to_ids=True, # Filter not working
-    deleted=False,
-    timestamp=last_days_unix,
-    order="timestamp desc", # Filter not working asc/desc
-    limit=config["export"]["last_ioc"],
-    pythonify=False,
-)
-print("Most recent IoC: ", time.ctime(int(res["Attribute"][0]["timestamp"])))
-print("Most old IoC:    ", time.ctime(int(res["Attribute"][-1]["timestamp"])))
+data = {
+    "controller": "attributes",
+    "type": ["ip-src", "ip-dst"],
+    "to_ids": True,
+    "deleted": False,
+    "timestamp": last_days_unix,
+    "order": "timestamp desc",
+    "limit": config["export"]["last_ioc"],
+}
+url = f"{config['misp']['url']}/attributes/restSearch"
+req = requests.post(url, json=data, **params_post)
+req.raise_for_status()
+print("Most recent IoC: ", time.ctime(int(req.json()["response"]["Attribute"][0]["timestamp"])))
+print("Most old IoC:    ", time.ctime(int(req.json()["response"]["Attribute"][-1]["timestamp"])))
 
 
 # Parse the result
-for item in res["Attribute"]:
+for item in req.json()["response"]["Attribute"]:
     if item["value"] not in ip_list:
         ip_list.append(item["value"])
 ioc_page = "\n".join(ip_list).encode("utf-8")
