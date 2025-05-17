cc3e9ac19b9dcee37e7457b25c13255898bf2465
Andrea Dainese <andrea.dainese@pm.me>
2025-05-17 14:56:54 +0200

    update

---
+++ Palo Alto Networks/Strata/get_arp.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from getpass import getpass
+from netmiko import ConnectHandler
+
+device = {
+    "device_type": "paloalto_panos",
+    "ip": "172.25.10.4",
+    "username": "admin",
+    "password": getpass(),
+    "fast_cli": False,
+}
+net_connect = ConnectHandler(**device)
+result = net_connect.send_command("show arp all", use_textfsm=True)
+print(result)
+net_connect.disconnect()
