326c746da986d9dd87a41d01bcd6e2038f7e3b77
Andrea Dainese <andrea.dainese@pm.me>
2025-05-18 10:22:29 +0200

    update

---
+++ Palo Alto Networks/Strata/get_address_rest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from getpass import getpass
+import requests
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "X-PAN-KEY": getpass(),
+}
+
+# Getting configured elements
+url = f"https://172.25.10.4/restapi/v10.2/Objects/Addresses?location=vsys&vsys=vsys1"
+req = requests.get(url, headers=headers, verify=False)
+req.raise_for_status()
+
+for entry in req.json()["result"]["entry"]:
+    print(f"{entry['@name']},{entry['ip-netmask']},{entry['@location']}")
