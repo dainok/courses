1c22321b190fb1f98debd714d94975a27991d340
Andrea Dainese <andrea.dainese@pm.me>
2025-06-17 12:55:00 +0200

    update

---
+++ Palo Alto Networks/Strata/get_arp_sdk_pan.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from getpass import getpass
+import ssl
+from pan import xapi
+
+token = getpass("Token: ")
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+xapi = xapi.PanXapi(hostname="172.24.1.34", api_key=token, ssl_context=ctx)
+
+# Getting elements
+xapi.op(cmd="<show><arp><entry name='all'/></arp></show>", cmd_xml=False)
+root = xapi.element_root
+for entry in root.iterfind(".//result/entries/"):
+    interface = entry.find("interface").text
+    ip = entry.find("ip").text
+    mac = entry.find("mac").text
+    print(f"{interface},{ip},{mac}")
