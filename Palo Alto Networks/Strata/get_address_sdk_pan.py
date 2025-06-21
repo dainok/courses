c2bb60246e2e045b9920c94760c556e7a171c749
Andrea Dainese <andrea.dainese@pm.me>
2025-06-21 10:37:23 +0200

    update

--- Palo Alto Networks/Strata/get_address_sdk_pan.py
+++ Palo Alto Networks/Strata/get_address_sdk_pan.py
@@ -5,7 +5,6 @@ import xml.etree.ElementTree as ET
 from pan import xapi
 
 token = getpass("Token: ")
-token = "LUFRPT1EeVlJTjIwQmZtVm9HTWJCdHowVXhQZzJnZUE9VGZhak9CaDNnSzg1M3dsT3Z4dEp4dEI2YllRQmRVY2EwQ3FJcEd6YWNrUlo4MThZZVlaTGxMakFmMVIrYlNpYw=="
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
