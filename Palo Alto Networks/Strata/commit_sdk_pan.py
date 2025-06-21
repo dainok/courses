604b6977a3b5c2443423cec6738b5eeb9de561c6
Andrea Dainese <andrea.dainese@pm.me>
2025-06-21 15:57:38 +0200

    update

---
+++ Palo Alto Networks/Strata/commit_sdk_pan.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from getpass import getpass
+import ssl
+from pan import xapi
+
+token = getpass("Token: ")
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+fw = xapi.PanXapi(hostname="172.24.1.34", api_key=token, ssl_context=ctx)
+
+# Commit
+cmd = "<commit></commit>"
+fw.commit(cmd=cmd, sync=True)
+if "no changes to commit" in fw.status_detail:
+    pass
+elif fw.status != "success" or "successfully" not in fw.status_detail:
+    print("ERROR: failed to commit.")
